02172585303cab4a17956d18bab37b4d3534db6d
pingzing <mcalistern@gmail.com>
2018-08-19 23:38:35 +0300

    Actually make AzCopy use SetContentType

--- pre-push.ps1
+++ pre-push.ps1
@@ -3,6 +3,6 @@ pelican content -o output -s publishconf.py
 Write-Host "Publish-ready version of site created. Copying to Azure Storage...`n"
 
 # Env var is user-specific 
-& 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y
+& 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y /SetContentType
 
 Write-Host "`n Copied to Azure storage. Pushing to git.`n"
