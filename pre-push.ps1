8b2bcbdc2b1b9ea6879cf4b5c9f48e47bdeb85d5
pingzing <mcalistern@gmail.com>
2018-08-19 23:18:59 +0300

    Make pre-push a little nicer

--- pre-push.ps1
+++ pre-push.ps1
@@ -1,4 +1,8 @@
 pelican content -o output -s publishconf.py
 
+Write-Host "Publish-ready version of site created. Copy to Azure Storage...`n"
+
 # Env var is user-specific 
 & 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y
+
+Write-Host
