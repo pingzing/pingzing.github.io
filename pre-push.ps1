75f44f43dd414d70c2e2e5c1e4a4ab171f60ecd8
pingzing <mcalistern@gmail.com>
2018-08-19 23:11:50 +0300

    Thwap env vars

--- pre-push.ps1
+++ pre-push.ps1
@@ -1,4 +1,4 @@
 pelican content -o output -s publishconf.py
 
-# Env var is user-specific
+# Env var is user-specific 
 & 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y
