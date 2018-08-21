66e7baf6c70b8c5c7b1ee46674c53360d2301aa3
pingzing <mcalistern@gmail.com>
2018-08-21 22:36:11 +0300

    Update push script to set CDN timeout for index.html, and change CSS to make links pretty

--- pre-push.ps1
+++ pre-push.ps1
@@ -3,6 +3,13 @@ pelican content -o output -s publishconf.py
 Write-Host "Publish-ready version of site created. Copying to Azure Storage...`n"
 
 # Env var is user-specific 
-& 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y /SetContentType
+& 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y /SetContentType /XO
 
-Write-Host "`n Copied to Azure storage. Pushing to git.`n"
+Write-Host "`n Copied to Azure storage. Setting cache-control of index.html...`n"
+
+$context = New-AzureStorageContext -StorageAccountName "travelneil" -StorageAccountKey $env:TravelNeilAzureKey
+$blob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "index.html"
+$blob.ICloudBlob.Properties.CacheControl = "max-age=300"
+$blob.ICloudBlob.SetPropertiesAsync()
+
+Write-Host "`n Index.html's max-age set, pushing to git...`n"
