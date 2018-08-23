40804e2770323d985e8ea14b8a423167cee7f7dd
Neil McAlister <neil.mcalister@futurice.com>
2018-08-23 17:57:49 +0300

    Update publish script

--- publish-to-azure.ps1
+++ publish-to-azure.ps1
@@ -5,11 +5,17 @@ Write-Host "Publish-ready version of site created. Copying to Azure Storage...`n
 # Env var is user-specific 
 & 'C:\Program Files (x86)\Microsoft SDKs\Azure\AzCopy\AzCopy.exe' /Source:./output '/Dest:https://travelneil.blob.core.windows.net/$web/' /DestKey:$env:TravelNeilAzureKey /S /Y /SetContentType /XO
 
-Write-Host "`n Copied to Azure storage. Setting cache-control of index.html...`n"
+Write-Host "`n Copied to Azure storage. Setting cache-control of index.html and main.css...`n"
 
 $context = New-AzureStorageContext -StorageAccountName "travelneil" -StorageAccountKey $env:TravelNeilAzureKey
-$blob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "index.html"
-$blob.ICloudBlob.Properties.CacheControl = "max-age=300"
-$dummy = $blob.ICloudBlob.SetPropertiesAsync().GetAwaiter().GetResult()
+$indexBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "index.html"
+$indexBlob.ICloudBlob.Properties.CacheControl = "max-age=300"
+$updateIndexMaxAgeTask = $indexBlob.ICloudBlob.SetPropertiesAsync()
+
+$mainCssBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "theme/css/main.css"
+$mainCssBlob.ICloudBlob.Properties.CacheControl = "max-age=300"
+$mainCssBlobUpdateTask = $mainCssBlob.ICloudBlob.SetPropertiesAsync()
+
+[System.Threading.Tasks.Task]::WaitAll($updateIndexMaxAgeTask, $mainCssBlobUpdateTask)
 
 Write-Host "Finished setting index.html's max age. Pushing to git...`n"
