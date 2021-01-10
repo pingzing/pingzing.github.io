d5ab7e6327cb020642678ddab642a9ecd20e348b
pingzing <mcalistern@gmail.com>
2021-01-11 00:17:33 +0200

    pdate publish script to update pygment.css as well

--- publish-to-azure.ps1
+++ publish-to-azure.ps1
@@ -1,21 +1,25 @@
-pelican content -o output -s publishconf.py 
+pelican content -o output -s publishconf.py;
 
-Write-Host "Publish-ready version of site created. Copying to Azure Storage..."
+Write-Host "Publish-ready version of site created. Copying to Azure Storage...";
 
 # Env var is an SAS-token that has full access to the $web resource in my azure account. It expires Dec 20, 2020. 
-& "C:\Users\mcali\azcopy\azcopy.exe" sync ./output "https://travelneil.blob.core.windows.net/`$web" --recursive --delete-destination true
+& "C:\Users\mcali\azcopy\azcopy.exe" sync ./output "https://travelneil.blob.core.windows.net/`$web" --recursive --delete-destination true;
 
-Write-Host "`nCopied to Azure storage. Setting cache-control of index.html and main.css...`n"
+Write-Host "`nCopied to Azure storage. Setting cache-control of index.html and main.css...`n";
 
-$context = New-AzureStorageContext -StorageAccountName "travelneil" -StorageAccountKey $env:TravelNeilAzureKey
-$indexBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "index.html"
-$indexBlob.ICloudBlob.Properties.CacheControl = "max-age=300"
-$updateIndexMaxAgeTask = $indexBlob.ICloudBlob.SetPropertiesAsync()
+$context = New-AzureStorageContext -StorageAccountName "travelneil" -StorageAccountKey $env:TravelNeilAzureKey;
+$indexBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "index.html";
+$indexBlob.ICloudBlob.Properties.CacheControl = "max-age=300";
+$updateIndexMaxAgeTask = $indexBlob.ICloudBlob.SetPropertiesAsync();
 
-$mainCssBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "theme/css/main.css"
-$mainCssBlob.ICloudBlob.Properties.CacheControl = "max-age=300"
-$mainCssBlobUpdateTask = $mainCssBlob.ICloudBlob.SetPropertiesAsync()
+$mainCssBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "theme/css/main.css";
+$mainCssBlob.ICloudBlob.Properties.CacheControl = "max-age=300";
+$mainCssBlobUpdateTask = $mainCssBlob.ICloudBlob.SetPropertiesAsync();
 
-[System.Threading.Tasks.Task]::WaitAll($updateIndexMaxAgeTask, $mainCssBlobUpdateTask)
+$pygmentCssBlob = Get-AzureStorageBlob -Context $context -Container '$web' -Blob "theme/css/pygment.css";
+$pygmentCssBlob.ICloudBlob.Properties.CacheControl = "max-age=300";
+$pygmentCssBlob = $pygmentCssBlob.ICloudBlob.SetPropertiesAsync();
 
-Write-Host "Finished setting index.html's and main.css's max age."
+[System.Threading.Tasks.Task]::WaitAll($updateIndexMaxAgeTask, $mainCssBlobUpdateTask, $pygmentCssBlob);
+
+Write-Host "Finished setting index.html's, main.css's and pygment.css's max age.";
